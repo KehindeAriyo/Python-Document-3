7b3088288663c289baa37eba205d869a9d4da505
Kehinde Ariyo <kehindeariyo7@gmail.com>
2024-06-13 17:10:41 -0500

    new file:   .ipynb_checkpoints/Wednesday_Lesson_Inclass_Updated_Lecture_Updated2021 (1)-checkpoint.ipynb 	new file:   Wednesday_Lesson_Inclass_Updated_Lecture_Updated2021 (1).ipynb 	new file:   __pycache__/circle.cpython-311.pyc 	new file:   __pycache__/module.cpython-311.pyc 	new file:   circle.py 	new file:   module.py

---
+++ circle.py
@@ -0,0 +1,4 @@
+import math   
+def calculate_circumference_of_a_circle(radius):
+    circumference = 2 * math.pi * radius
+    return circumference
---
+++ module.py
@@ -0,0 +1,3 @@
+def calculate_square_footage(length, width):
+    return length * width
+
